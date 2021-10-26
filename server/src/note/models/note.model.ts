271a790897ab56a42a2416e627c55d9573a49315
Gilbert <gilwong3105@gmail.com>
2021-10-26 01:31:43 -0700

    fixup! first commit

---
+++ server/src/note/models/note.model.ts
@@ -0,0 +1,35 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@ObjectType({ description: 'note' })
+@Entity()
+export class Note {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column()
+  title: string;
+
+  @Field()
+  @Column()
+  content: string;
+
+  @Field()
+  created_by: string;
+
+  @Field(() => Date)
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Field(() => Date)
+  @UpdateDateColumn()
+  updated_at: Date;
+}
