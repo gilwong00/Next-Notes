0b13aa0bddc7ac0febab8f394998bb0f2df733e9
Gilbert <gilwong3105@gmail.com>
2022-09-19 18:34:04 -0700

    Fix any types

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,16 +1,27 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import { AppPropsType, AppType } from 'next/dist/shared/lib/utils';
+import NextDocument, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext
+} from 'next/document';
+import { NextRouter } from 'next/router';
+import { PropsWithChildren } from 'react';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class Document extends NextDocument {
-  static async getInitialProps(ctx: any) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App: any) => (props: any) =>
-            sheet.collectStyles(<App {...props} />)
+          enhanceApp:
+            (App: AppType) =>
+            (props: PropsWithChildren<AppPropsType<NextRouter, {}>>) =>
+              sheet.collectStyles(<App {...props} />)
         });
 
       const initialProps = await NextDocument.getInitialProps(ctx);
